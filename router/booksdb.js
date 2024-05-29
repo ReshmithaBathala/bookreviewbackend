b9dc181284233568151d1247550240fc05527f84
ReshmithaBathala <reshmithabathala26@gmail.com>
2024-05-29 11:31:01 +0530

    Book review

---
+++ router/booksdb.js
@@ -0,0 +1,14 @@
+let books = {
+  1: { author: "Rabindranath Tagore", title: "Gitanjali", reviews: {} },
+  2: { author: "R.K. Narayan", title: "Malgudi Days", reviews: {} },
+  3: { author: "Salman Rushdie", title: "Midnight's Children", reviews: {} },
+  4: { author: "Arundhati Roy", title: "The God of Small Things", reviews: {} },
+  5: { author: "Vikram Seth", title: "A Suitable Boy", reviews: {} },
+  6: { author: "Anita Desai", title: "Clear Light of Day", reviews: {} },
+  7: { author: "Jhumpa Lahiri", title: "Interpreter of Maladies", reviews: {} },
+  8: { author: "Chetan Bhagat", title: "Five Point Someone", reviews: {} },
+  9: { author: "Amitav Ghosh", title: "The Glass Palace", reviews: {} },
+  10: { author: "Ruskin Bond", title: "The Blue Umbrella", reviews: {} },
+};
+
+module.exports = books;
